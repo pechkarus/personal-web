e5c76993ad01f688c87cb3f9ed8a203c63e318c2
Petr Kadomtsev <pechkarus@gmail.com>
2014-09-08 23:42:50 -0700

    Initial instagram integration

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var instagram = require('instagram-node-lib');
+var nconf = require('nconf');
+
 var router = express.Router();
+
+nconf
+  .file('secret.json')
+  .env();
+
+instagram.set('client_id', nconf.get('instagramClientId'));
+instagram.set('client_secret', nconf.get('instagramClientSecret'));
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Petr Kadomtsev' });
+  instagram.users.recent({ 
+	  user_id: 3256003,
+	  complete: function(data){
+		  res.render('index', { photos: data });
+	  }
+  });	  
 });
 
 module.exports = router;
